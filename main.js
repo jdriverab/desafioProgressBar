d2607deac8b9c093c818f014795d3251c2035dc5
jdriverab <jdriverab@hotmail.com>
2021-10-19 13:34:02 -0300

    Completed

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 let currentState = 0;
 let isNOTInPause = true;
+const cicleTime = 500;
 
-function squareFiller (x) { for(var i=0 ; i < x; i++){ document.getElementById(`square${i+1}`).style.backgroundColor = "black" }}
+/**
+ * Funcion para cambiar el color de los cuadrados de la progressBar a negro.
+ * @param {number} x Variable de entrada, indica el cuadro de la progressBar que se modificara.
+ * @return void
+ */
+function squareFiller (x) { for(var i=0 ; i < x; i++){ document.getElementById(`square${i+1}`).style.backgroundColor = "black" }};
+
+/**
+ * Funcion que realiza el ciclo segun "cicleTime" definido, en ciclo hace llamado de funcion squareFiller
+ * 
+ */
+function cicleFiller () {
+    setTimeout(()=> squareFiller(currentState),cicleTime);
+    if(isNOTInPause && currentState <= 14){ currentState ++;setTimeout(()=>cicleFiller(),cicleTime)}
+}
 
 function restartSquares (){
-    for(var i= 0; i <= 14; i++){
-        document.getElementById(`square${i+1}`).style.backgroundColor = "white"
-    }
-    currentState = 0
+    for(var i= 0; i <= 14; i++){ document.getElementById(`square${i+1}`).style.backgroundColor = "white" };
+    currentState = 0;
+    isNOTInPause = false
 }
 
-const start = () => {
-    isNOTInPause= true
-    if(currentState <= 14){
-        let intervalID = setInterval(()=>{
-            if(isNOTInPause){     
-                squareFiller(currentState)
-                currentState ++
-                if(currentState === 14) {clearInterval(intervalID)}
-            }   
-        },500)
-        console.log(currentState)
-    } 
-    console.log(currentState)
-}
+const start = () => { isNOTInPause= true,cicleFiller() };
 
-const pause = () => { isNOTInPause === false ? isNOTInPause = true : isNOTInPause = false }
+const pause = () => { isNOTInPause === false ? isNOTInPause = true : isNOTInPause = false };
 
-const restart = () =>{ restartSquares () }
-
+const restart = () =>{ restartSquares () };
