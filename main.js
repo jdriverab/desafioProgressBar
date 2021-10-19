4d557549eea3292d1ad474b01fb1d15ba3c8c92e
jdriverab <jdriverab@hotmail.com>
2021-10-18 23:17:50 -0300

    queda bug por mal ciclo

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-let currentState = 0
-const states = []
-const increase = 0.5
+let currentState = 0;
+let isNOTInPause = true;
 
-function start () {
-    squareFiller ()
-
-}
-
-function pause () {
-
-}
-
-function restart (){
-
-    restartSquares ()
-
-}
-
-function squareFiller () {
-    for(let i= 0; i < currentState; i++){
-        document.getElementById(`square${i+1}`).style.backgroundColor = "black"
-    }
-}
+function squareFiller (x) { for(var i=0 ; i < x; i++){ document.getElementById(`square${i+1}`).style.backgroundColor = "black" }}
 
 function restartSquares (){
-    for(let i= 0; i <= 14; i++){
+    for(var i= 0; i <= 14; i++){
         document.getElementById(`square${i+1}`).style.backgroundColor = "white"
     }
+    currentState = 0
 }
+
+const start = () => {
+    isNOTInPause= true
+    if(currentState <= 14){
+        let intervalID = setInterval(()=>{
+            if(isNOTInPause){     
+                squareFiller(currentState)
+                currentState ++
+                if(currentState === 14) {clearInterval(intervalID)}
+            }   
+        },500)
+        console.log(currentState)
+    } 
+    console.log(currentState)
+}
+
+const pause = () => { isNOTInPause === false ? isNOTInPause = true : isNOTInPause = false }
+
+const restart = () =>{ restartSquares () }
 
